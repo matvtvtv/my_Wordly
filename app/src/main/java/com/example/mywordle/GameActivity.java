15d164be0dd8b7fc37dfb4d5aae13b1c10d8200f
Matvei <matveicharniauski@gmail.com>
2025-03-07 00:01:00 +0300

    game Reg

--- app/src/main/java/com/example/mywordle/GameActivity.java
+++ app/src/main/java/com/example/mywordle/GameActivity.java
@@ -1,28 +1,31 @@
 package com.example.mywordle;
 
-import android.media.MediaPlayer;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.mywordle.Adapter.KeyboardAdapter;
+
 import com.example.mywordle.Keyboard.Keyboard;
 import com.example.mywordle.data.model.WordsModel;
-import com.example.mywordle.data.repository.WordsFileReader;
+import com.example.mywordle.data.repository.DatabaseHelper;
+import com.example.mywordle.data.repository.WordsRepository;
 import com.example.mywordle.databinding.ActivityGameBinding;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
+    // Инициализируем ViewBinding
     ActivityGameBinding binding;
-    private ImageView exit_button;
     private GridLayout gridLetters;
     private List<TextView> letterCells;
     private int currentCellIndex = 0;
@@ -30,71 +33,42 @@ public class GameActivity extends AppCompatActivity {
     private int wordLength;
     private final int MAX_ATTEMPTS = 6;
     private GameLogic gameLogic;
-    private Keyboard keyboard;
-    private WordsFileReader fileReader;
+    private WordsRepository wordsRepository;
 
-    // Инициализация клавиатуры
-    private final List<Keyboard.Key> keyList = java.util.Arrays.asList(
-            new Keyboard.Key("Й"),
-            new Keyboard.Key("Ц"),
-            new Keyboard.Key("У"),
-            new Keyboard.Key("К"),
-            new Keyboard.Key("Е"),
-            new Keyboard.Key("Н"),
-            new Keyboard.Key("Г"),
-            new Keyboard.Key("Ш"),
-            new Keyboard.Key("Щ"),
-            new Keyboard.Key("З"),
-            new Keyboard.Key("Х"),
-            new Keyboard.Key("Ъ"),
-            new Keyboard.Key("Ф"),
-            new Keyboard.Key("Ы"),
-            new Keyboard.Key("В"),
-            new Keyboard.Key("А"),
-            new Keyboard.Key("П"),
-            new Keyboard.Key("Р"),
-            new Keyboard.Key("О"),
-            new Keyboard.Key("Л"),
-            new Keyboard.Key("Д"),
-            new Keyboard.Key("Ж"),
-            new Keyboard.Key("Э"),
-            new Keyboard.Key("Я"),
-            new Keyboard.Key("Ч"),
-            new Keyboard.Key("С"),
-            new Keyboard.Key("М"),
-            new Keyboard.Key("И"),
-            new Keyboard.Key("Т"),
-            new Keyboard.Key("Б"),
-            new Keyboard.Key("Ю"),
-            new Keyboard.Key("Ь"),
+    // Инициализация клавиш для клавиатуры
+    private List<Keyboard.Key> keyList = java.util.Arrays.asList(
+            new Keyboard.Key("Й"), new Keyboard.Key("Ц"), new Keyboard.Key("У"), new Keyboard.Key("К"),
+            new Keyboard.Key("Е"), new Keyboard.Key("Н"), new Keyboard.Key("Г"), new Keyboard.Key("Ш"),
+            new Keyboard.Key("Щ"), new Keyboard.Key("З"), new Keyboard.Key("Х"), new Keyboard.Key("Ъ"),
+            new Keyboard.Key("Ф"), new Keyboard.Key("Ы"), new Keyboard.Key("В"), new Keyboard.Key("А"),
+            new Keyboard.Key("П"), new Keyboard.Key("Р"), new Keyboard.Key("О"), new Keyboard.Key("Л"),
+            new Keyboard.Key("Д"), new Keyboard.Key("Ж"), new Keyboard.Key("Э"), new Keyboard.Key("Я"),
+            new Keyboard.Key("Ч"), new Keyboard.Key("С"), new Keyboard.Key("М"), new Keyboard.Key("И"),
+            new Keyboard.Key("Т"), new Keyboard.Key("Б"), new Keyboard.Key("Ю"), new Keyboard.Key("Ь"),
             new Keyboard.Key("Del", LetterStatus.GRAY)
     );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Инициализация ViewBinding
         binding = ActivityGameBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Получаем параметр длины слова через Intent
         wordLength = getIntent().getIntExtra("WORD_LENGTH", 5);
-        // Получаем сложность слова. Если сложность не требуется, передавайте -1
-        int wordDifficulty = getIntent().getIntExtra("WORD_DIFFICULTY", -1);
 
-        // Инициализируем WordsFileReader
-        fileReader = new WordsFileReader(this);
+        // Инициализация репозитория для работы с базой данных
+        DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        wordsRepository = new WordsRepository(db);
 
-        // Универсальная фильтрация: если wordDifficulty < 0, фильтруем только по длине,
-        // иначе по сложности и длине.
-        List<WordsModel> validWords = fileReader.getFilteredWords(wordLength, wordDifficulty);
-        if (validWords.isEmpty()) {
-            String msg = wordDifficulty < 0
-                    ? "Нет слов с длиной " + wordLength + " букв!"
-                    : "Нет слов с сложностью " + wordDifficulty + " и длиной " + wordLength + " букв!";
-            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
+        // Получаем список допустимых слов из базы данных
+        List<WordsModel> validWords = wordsRepository.getFilteredWordsFree(wordLength); // -1 для игнорирования сложности
+
+
+
         // Выбираем случайное слово
         int randomIndex = (int) (Math.random() * validWords.size());
         WordsModel selectedWord = validWords.get(randomIndex);
@@ -135,20 +109,15 @@ public class GameActivity extends AppCompatActivity {
             gridLetters.addView(cell);
             letterCells.add(cell);
         }
-
         GridLayout layout = findViewById(R.id.gridLetters);
         layout.setColumnCount(wordLength);
 
-        exit_button = findViewById(R.id.exit_button);
-        exit_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+
+        // Обработчик кнопки выхода
+        binding.exitButton.setOnClickListener(v -> finish());
 
         // Настройка клавиатуры
-        keyboard = new Keyboard(findViewById(R.id.keyboard), keyList);
+        Keyboard keyboard = new Keyboard(binding.keyboard, keyList);
         keyboard.setOnKeyClickListener(v -> {
             Button btn = (Button) v;
             String letter = btn.getText().toString();
@@ -167,72 +136,66 @@ public class GameActivity extends AppCompatActivity {
         keyboard.create(this, binding.getRoot());
 
         // Обработка нажатия кнопки "Проверить"
-        Button btnCheck = findViewById(R.id.btnCheck);
-        btnCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button btnCheck = binding.btnCheck;
+        btnCheck.setOnClickListener(v -> {
+            int start = currentAttemptIndex * wordLength;
+            int end = start + wordLength;
+            StringBuilder guessBuilder = new StringBuilder();
+            for (int i = start; i < end; i++) {
+                guessBuilder.append(letterCells.get(i).getText());
+            }
+            String guess = guessBuilder.toString().trim();
 
-                int start = currentAttemptIndex * wordLength;
-                int end = start + wordLength;
-                StringBuilder guessBuilder = new StringBuilder();
-                for (int i = start; i < end; i++) {
-                    guessBuilder.append(letterCells.get(i).getText());
-                }
-                String guess = guessBuilder.toString().trim();
+            if (guess.length() != wordLength) {
+                Toast.makeText(getApplicationContext(), "Введите слово из " + wordLength + " букв!", Toast.LENGTH_SHORT).show();
+                return;
+            }
 
-                if (guess.length() != wordLength) {
-                    Toast.makeText(getApplicationContext(), "Введите слово из " + wordLength + " букв!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                // Проверка: существует ли введённое слово в файле
-                if (!fileReader.isValidWord(guess)) {
+            // Проверка: существует ли введённое слово в базе данных
+            try {
+                if (!wordsRepository.isValidWord(guess)) {
                     Toast.makeText(getApplicationContext(), "Похоже, я не знаю такого слова", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                try {
-                    AttemptResult result = gameLogic.checkWord(guess);
-                    for (int i = 0; i < wordLength; i++) {
-                        TextView cell = letterCells.get(start + i);
-                        Keyboard.Key key = keyboard.findByKeyText(String.valueOf(result.getGuess().charAt(i)));
-                        switch (result.getStatuses()[i]) {
-                            case GREEN:
-                                if (key != null && (key.getStatus() == LetterStatus.UNDEFINED || key.getStatus() == LetterStatus.YELLOW)) {
-                                    key.setStatus(LetterStatus.GREEN);
-                                    keyboard.notifyKeyChanged(key);
-                                }
-                                cell.setBackgroundResource(R.drawable.cell_background_green);
-                                break;
-                            case YELLOW:
-                                if (key != null && key.getStatus() == LetterStatus.UNDEFINED) {
-                                    key.setStatus(LetterStatus.YELLOW);
-                                    keyboard.notifyKeyChanged(key);
-                                }
-                                cell.setBackgroundResource(R.drawable.cell_background_yellow);
-                                break;
-                            case GRAY:
-                                if (key != null && key.getStatus() == LetterStatus.UNDEFINED) {
-                                    key.setStatus(LetterStatus.GRAY);
-                                    keyboard.notifyKeyChanged(key);
-                                }
-                                cell.setBackgroundResource(R.drawable.cell_background_grey);
-                                break;
-                        }
+                AttemptResult result = gameLogic.checkWord(guess);
+                for (int i = 0; i < wordLength; i++) {
+                    TextView cell = letterCells.get(start + i);
+                    Keyboard.Key key = keyboard.findByKeyText(String.valueOf(result.getGuess().charAt(i)));
+                    switch (result.getStatuses()[i]) {
+                        case GREEN:
+                            if (key != null && (key.getStatus() == LetterStatus.UNDEFINED || key.getStatus() == LetterStatus.YELLOW)) {
+                                key.setStatus(LetterStatus.GREEN);
+                                keyboard.notifyKeyChanged(key);
+                            }
+                            cell.setBackgroundResource(R.drawable.cell_background_green);
+                            break;
+                        case YELLOW:
+                            if (key != null && key.getStatus() == LetterStatus.UNDEFINED) {
+                                key.setStatus(LetterStatus.YELLOW);
+                                keyboard.notifyKeyChanged(key);
+                            }
+                            cell.setBackgroundResource(R.drawable.cell_background_yellow);
+                            break;
+                        case GRAY:
+                            if (key != null && key.getStatus() == LetterStatus.UNDEFINED) {
+                                key.setStatus(LetterStatus.GRAY);
+                                keyboard.notifyKeyChanged(key);
+                            }
+                            cell.setBackgroundResource(R.drawable.cell_background_grey);
+                            break;
                     }
-
-                    if (gameLogic.isGameWon()) {
-                        Toast.makeText(getApplicationContext(), "Поздравляем, вы выиграли!", Toast.LENGTH_LONG).show();
-                        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.e377e9b8d135e68);
-                        mediaPlayer.start();
-                    } else if (gameLogic.isGameOver()) {
-                        Toast.makeText(getApplicationContext(), "Попытки закончились! Загаданное слово: " + gameLogic.getHiddenWord(), Toast.LENGTH_LONG).show();
-                    } else {
-                        currentAttemptIndex++;
-                    }
-                } catch (IllegalStateException e) {
-                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
+
+                if (gameLogic.isGameWon()) {
+                    Toast.makeText(getApplicationContext(), "Поздравляем, вы выиграли!", Toast.LENGTH_SHORT).show();
+                } else if (gameLogic.isGameOver()) {
+                    Toast.makeText(getApplicationContext(), "Попытки закончились! Загаданное слово: " + gameLogic.getHiddenWord(), Toast.LENGTH_SHORT).show();
+                } else {
+                    currentAttemptIndex++;
+                }
+            } catch (IllegalStateException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/mywordle/MainActivity.java
+++ app/src/main/java/com/example/mywordle/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.mywordle;
 
+import static java.security.AccessController.getContext;
+
+import android.animation.ArgbEvaluator;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.animation.AnimatorSet;
-import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import com.example.mywordle.data.repository.DatabaseHelper;
+import com.example.mywordle.data.repository.WordsRepository;
 import com.example.mywordle.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,105 +35,96 @@ public class MainActivity extends AppCompatActivity {
     private ImageView settings_button_main;
     private ImageView home_button_main;
     private ImageView profile_button_main;
-    private int frame = 1;
+
+    private int frame = 1; // 0 - profile, 1 - home, 2 - settings
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        WordsRepository wordsRepository = new WordsRepository(db);
+        // Enable edge-to-edge content
+         SharedPreferences preferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+                boolean isFirstRun = preferences.getBoolean("isFirstRun", true);
+         if (isFirstRun) {
+             wordsRepository.importWordsFromFile(this);
+                    // Если это первый запуск, открываем RegistrationActivity
+             Intent intent = new Intent(getContext(), com.example.mywordle.ui.RegistrationActivity.class);
+             startActivity(intent);
+
+                    // После первого запуска изменяем флаг, чтобы не запускать RegistrationActivity снова
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("isFirstRun", false);
+                    editor.apply();
+                }
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Set up the window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Create an instance of DatabaseHelper for accessing the database
+
+        // Create an instance of WordsRepository and pass the context
+
+
+        if(wordsRepository.isTableEmpty()){
+
+        }
+
+        // Initialize view binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        getAllId();
+        setContentView(binding.getRoot());
+
+        getAllId(); // Get references to the buttons
+
+
+
+
+
+        // Change fragment initially to FragmentMain
         change(new FragmentMain());
         home_button_main.setScaleX(1.5f);
         home_button_main.setScaleY(1.5f);
 
-        settings_button_main.setOnClickListener(v -> {
-            Animation clickAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click);
-            v.startAnimation(clickAnim);
-            clickAnim.setAnimationListener(new Animation.AnimationListener() {
-                @Override public void onAnimationStart(Animation animation) { }
-                @Override public void onAnimationRepeat(Animation animation) { }
-                @Override
-                public void onAnimationEnd(Animation animation) {
-
-                    if (frame != 2) {
-                        resetIcons(home_button_main,0);
-                        resetIcons(profile_button_main,0);
-                        animIcon(v);
-
-                        change(new FragmentSettings());
-                        frame = 2;
-                    }
-                    else{resetIcons(home_button_main,300);
-                         resetIcons(profile_button_main,300);
-
-                    }
-
-                }
-            });
-        });
-
-        home_button_main.setOnClickListener(v -> {
-            Animation clickAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click);
-            v.startAnimation(clickAnim);
-            clickAnim.setAnimationListener(new Animation.AnimationListener() {
-                @Override public void onAnimationStart(Animation animation) { }
-                @Override public void onAnimationRepeat(Animation animation) { }
-                @Override
-                public void onAnimationEnd(Animation animation) {
-
-                    if (frame != 1) {
-                        animIcon(v);
-                        resetIcons(profile_button_main,0);
-                        resetIcons(settings_button_main,0);
-
-                        change(new FragmentMain());
-                        frame = 1;
-                    }
-                     else{   resetIcons(profile_button_main,300);
-                        resetIcons(settings_button_main,300);
-                        }
-                }
-            });
-        });
-
-        profile_button_main.setOnClickListener(v -> {
-
-            Animation clickAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click);
-            v.startAnimation(clickAnim);
-            clickAnim.setAnimationListener(new Animation.AnimationListener() {
-                @Override public void onAnimationStart(Animation animation) { }
-                @Override public void onAnimationRepeat(Animation animation) { }
-                @Override
-                public void onAnimationEnd(Animation animation) {
-
-                    if (frame != 0) {
-                        resetIcons(home_button_main,0);
-                        resetIcons(settings_button_main,0);
-                        animIcon(v);
-                        change(new FragmentProfile());
-                        frame = 0;
-                    }
-                    else{resetIcons(home_button_main,300);
-                        resetIcons(settings_button_main,300);
-                    }
-                }
-            });
-
-        });
-
-
-
+        // Set up listeners for buttons with animations
+        settings_button_main.setOnClickListener(v -> handleFragmentChange(v, new FragmentSettings(), 2));
+        home_button_main.setOnClickListener(v -> handleFragmentChange(v, new FragmentMain(), 1));
+        profile_button_main.setOnClickListener(v -> handleFragmentChange(v, new FragmentProfile(), 0));
     }
 
+    // Generic method for changing fragments and handling icon animations
+    private void handleFragmentChange(View v, Fragment fragment, int targetFrame) {
+        Animation clickAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click);
+        v.startAnimation(clickAnim);
+        clickAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override public void onAnimationStart(Animation animation) { }
+            @Override public void onAnimationRepeat(Animation animation) { }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (frame != targetFrame) {
+                    resetIcons(home_button_main, 0);
+                    resetIcons(profile_button_main, 0);
+                    resetIcons(settings_button_main, 0);
+                    animIcon(v); // Animate the icon for the clicked button
+                    change(fragment); // Change the fragment
+                    frame = targetFrame; // Update the frame state
+                } else {
+                    resetIcons(home_button_main, 300);
+                    resetIcons(profile_button_main, 300);
+                    resetIcons(settings_button_main, 300);
+                }
+            }
+        });
+    }
+
+    // Change the displayed fragment
     private void change(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.setCustomAnimations(R.anim.fragment_in, R.anim.fragment_out);
@@ -135,46 +132,41 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    // Get references to the buttons
     private void getAllId() {
         settings_button_main = findViewById(R.id.settings_button_main);
         home_button_main = findViewById(R.id.home_button_main);
         profile_button_main = findViewById(R.id.profile_button_main);
-
     }
 
-
-
+    // Reset icon's color and size when switching
     @SuppressLint("UseCompatLoadingForDrawables")
-    private void resetIcons(View v, int i) {
-        // Сброс цветовых фильтров и масштабов для всех иконок
-
-
+    private void resetIcons(View v, int duration) {
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(v, "scaleX", 1.5f, 1f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(v, "scaleY", 1.5f, 1f);
-        scaleX.setDuration(i);
-        scaleY.setDuration(i);
+        scaleX.setDuration(duration);
+        scaleY.setDuration(duration);
 
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(scaleX, scaleY);
         animatorSet.start();
     }
 
+    // Animate the selected icon (enlarge and change color)
     private void animIcon(View v) {
-        // Анимация увеличения (scale up)
         ObjectAnimator scaleX = ObjectAnimator.ofFloat(v, "scaleX", 1f, 1.5f);
         ObjectAnimator scaleY = ObjectAnimator.ofFloat(v, "scaleY", 1f, 1.5f);
         scaleX.setDuration(300);
         scaleY.setDuration(300);
 
-        // Анимация перекрашивания
         ValueAnimator colorAnimator = getColorAnimator(v, R.color.original_color, R.color.pressed_color);
 
-        // Объединяем анимации в AnimatorSet
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(scaleX, scaleY, colorAnimator);
         animatorSet.start();
     }
 
+    // Create a color transition animation
     private ValueAnimator getColorAnimator(View v, @ColorRes int from, @ColorRes int to) {
         int startColor = ContextCompat.getColor(this, from);
         int endColor = ContextCompat.getColor(this, to);
@@ -186,6 +178,4 @@ public class MainActivity extends AppCompatActivity {
         });
         return colorAnimator;
     }
-
-
 }
