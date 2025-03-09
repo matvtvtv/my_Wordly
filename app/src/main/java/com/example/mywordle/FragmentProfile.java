2ba6092bf11e96f1977590c42860b26cc088e99a
Matvei <matveicharniauski@gmail.com>
2025-03-09 12:44:35 +0300

    game profile

--- app/src/main/java/com/example/mywordle/FragmentProfile.java
+++ app/src/main/java/com/example/mywordle/FragmentProfile.java
@@ -5,17 +5,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.mywordle.data.model.PlayerModel;
+import com.example.mywordle.data.repository.PlayerRepository;
+
 
 public class FragmentProfile extends Fragment {
+    private TextView allGamesText;
 
 @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup contaner, Bundle savedInstanceState){
     View view = inflater.inflate(R.layout.fragment_profile,contaner,false);
+    getAllId(view);
+    PlayerRepository playerRepository = PlayerRepository.getInstance(getContext());
+    int userId = playerRepository.getCurrentUserId();
+    PlayerModel user = playerRepository.getUserData(userId);
+    allGamesText.setText(String.valueOf(user.getAllGames()));
+    playerRepository.addOnDataUpdateListener(values -> {
+        Integer allGames = (Integer) values.get("allGames");
+        if (allGames != null) {
+            allGamesText.setText(String.valueOf(allGames));
+        }
+    });
     return view;
+
 }
+    private void getAllId(View view) {
+        allGamesText       = view.findViewById(R.id.all_games_text);
+
+    }
 
 
 }
